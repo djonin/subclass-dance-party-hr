bdd24472e6b9cac15e2e95d838dce168e615e577
unknown <delme@bootcamp2.delcam.local>
2015-07-17 14:27:19 -0700

    mousemove

--- src/snakeyDancer.js
+++ src/snakeyDancer.js
@@ -1,8 +1,19 @@
 function SnakeyDancer(top, left, timeBetweenSteps, minRadius, maxRadius) {
-  Dancer.call(this, top, left, timeBetweenSteps);
   this.timer = null;
   this.speed = 20;
   this.count = 1;
+  this.goTo = {
+    x: $(window).width() / 2,
+    y: $(window).height() / 2
+  };
+
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  var that = this;
+  $(window).mousemove(function(event) {
+    that.goTo.x = event.pageX;
+    that.goTo.y = event.pageY;
+  });
 }
 
 SnakeyDancer.prototype = Object.create(Dancer.prototype);
@@ -14,13 +25,11 @@ SnakeyDancer.prototype.step = function() {
 
   var copy = this.$node
   var that = this;
-  var top =
-    Number.parseInt(this.$node.css('top')) +
-    (Math.random() > 0.5 ? 1 : -1) * Math.random() * this.speed
+  var top = Number.parseInt(this.$node.css('top'));
+  top = top + (this.goTo.y - top) * Math.random() /2 + (Math.random() *2 -1) * 10 ;
 
-  var left =
-    Number.parseInt(this.$node.css('left')) +
-    (Math.random() > 0.5 ? 1 : -1) * Math.random() * this.speed;
+  var left = Number.parseInt(this.$node.css('left'));
+  left = left + (this.goTo.x - left ) * Math.random()/2 + (Math.random() *2 -1) * 10 ;
 
   var styleSettings = {
     top: top,
@@ -34,17 +43,21 @@ SnakeyDancer.prototype.step = function() {
 
   this.count++;
 
-  var color = 255 / this.count;
+  var color = this.count * 10 % 255;
 
   this.$node.css(
     'border-color',
-    'rgba(' + color + ', ' + Math.floor(color/ 2) + ', ' + color + ', ' + Math.random() +')');
+    'rgba('
+     + color + ', '
+     + Math.round(Math.random() ) * color + ', '
+     + Math.round(Math.random() ) * color + ', ' +
+    Math.random() + ')');
 
   setTimeout(function() {
     copy.remove();
     that.count--;
     that.timer = null;
-  }, Math.random() * 1000);
+  }, Math.random() * 5000);
 };
 
 function makeSnakeyDancer(top, left, timeBetweenSteps) {
